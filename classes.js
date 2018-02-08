5408f52a3cfd65a65c52cb69fc38de46ea6c410a
engie15 <e.matene15@gmail.com>
2018-02-08 13:38:10 +1100

    Practiced events making div four visible using JS

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()  
   three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -30,3 +31,7 @@ function three () {
   three.classList.toggle('yellow')
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')
+  makeVisible[0].classList.toggle('visible')
+}
